e89b5948b8e63ba96618879da035e401b4dcea0d
Dodanto <ohho0728@naver.com>
2019-11-27 20:46:52 +0900

    Update sound_editing.py

--- sound_editing.py
+++ sound_editing.py
@@ -15,3 +15,22 @@ def snap_count(filepath, hop=500, cp=0.25):
 
 def snap_frames_to_time(snap_frames, sr=22050):
     return librosa.core.frames_to_time(snap_frames, sr=sr, hop_length=1)
+
+def cut_samples(filepath):
+    y, sr = librosa.load(filepath)
+    _, snap_frames = snap_count(filepath)
+    sample_ls = []
+    for i in snap_frames:
+        sample_ls.append(y[i-512:i+513])
+    return sample_ls
+
+
+
+snap_frames = [512*2]
+sd = librosa.core.frames_to_time(snap_frames, sr=22050, hop_length=1)
+sd
+
+filepath = '30_times.wav'
+ls = cut_samples(filepath)
+len(ls)
+type(ls)
